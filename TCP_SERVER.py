e17c15bcd2163d04e28309108bf9d955db8ded7f
charl <carlos.nunez6@upr.edu>
2021-09-08 21:13:55 -0500

    Comment corrections as well

--- TCP_SERVER.py
+++ TCP_SERVER.py
@@ -13,7 +13,7 @@ print("[*] Listening on %s:%d" % (bind_ip, bind_port))
 
 
 # Opens the Data.txt file and returns all the content in it.
-def readFile(filename):
+def readfile(filename):
     info = ""
     file = open(filename)
     for line in file:
@@ -22,23 +22,23 @@ def readFile(filename):
 
 
 # Client/Server receives the information sent by the user then proceeds to save it in the existing file.
-# Once finished it lets sends a message to the user letting them know that the data was received and saved.
+# Once finished it sends a message to the user letting them know that the data was received and saved.
 def handler_client(client_socket):
 
     request = client_socket.recv(4096)
-    text = readFile("Data")
+    text = readfile("Data")
     file = open("Data", "w")
 
     file.write(text + "\n" + request.decode("utf-8"))
     file.close()
 
-    client_socket.send(b"Username and Password Saved!")
+    client_socket.send(b"Connection Successful and Username and Password Saved!")
     client_socket.close()
 
 
 # This method enables the server program to not end, thus constantly listening and waiting for a client to connect.
 # Once a client connects the program is enabled to finish once the user is finished with the connection.
-def StandBy():
+def stand_by():
     while True:
         client, addr = server.accept()
         print("[*] Accepted connection from: %s %d" % (addr[0], addr[1]))
@@ -46,4 +46,5 @@ def StandBy():
         client_handler.start()
 
 
-StandBy()
+stand_by()
+exit()
--- Tcp.Client2.py
+++ Tcp.Client2.py
@@ -1,16 +1,32 @@
 import socket
 
+# Where the client will connect
 target = "localhost"
 port = 9999
 
+
+# Client being created
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Client connecting to the server
 client.connect((target, port))
+
+# Asking the user for a Username and Password
+print(" -------------------------")
+print("|  TCP CLIENT CONNECTION  |")
+print(" -------------------------\n")
 inputUsername = input("Username: ")
 inputPassword = input("Password: ")
+
+# Encodes the Data(Username and Password) to be sent to the Server
 infoToSend = inputUsername + ";"+inputPassword
 client.send(infoToSend.encode("utf-8"))
 
+# Response that the Server sends back to the Client to prove the connection was successful
 response = client.recv(4096)
 
+
+# Prints the response received from the Server
 print(response.decode("utf-8"))
+
+exit()
